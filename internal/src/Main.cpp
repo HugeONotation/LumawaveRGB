d35fe2d3e284406083ad4cc8e35c60e675d0df36
HugeONotation <HugeONotation@gmail.com>
2024-07-16 13:50:00 -0700

    Initial commit

---
+++ internal/src/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    //TODO: Get audio inputs
+    //TODO: Execute light program
+    //TODO: Communicate with LED strip
+}
